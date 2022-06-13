dfe3286dcee45f7f8fd3b1655e893ee7418cf6a4
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-06-12 20:29:27 -0500

    Add kitten#new and kitten#create

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,4 +2,25 @@ class KittensController < ApplicationController
   def index
     @kittens = Kitten.all.order(:name)
   end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to kittens_path
+    else
+      flash.now[:alert] = 'Save failed.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
